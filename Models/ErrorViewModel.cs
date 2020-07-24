8e078dfe33ac02477a001d45be955b4cf7dbb1b7
John Uthus <john.a.uthus.jr1@gmail.com>
2020-07-24 13:31:36 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AutomobileWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
